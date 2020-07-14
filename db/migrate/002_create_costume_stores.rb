adaa5fd4444ed498d961be5796b2d11cddee1bc0
Kevin Boston <kbos22@gmail.com>
2020-07-14 14:02:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean :still+
-      t.integer :open_time
+      t.boolean :still_in_business
+      t.integer :opening_time
       t.integer :closing_time
       t.timestamps null: false
     end
